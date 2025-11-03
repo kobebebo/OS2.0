6e3f925a883f0438a6d980d66b530c31eed381ed
kobebebo <kobebebo51@gmail.com>
2025-11-03 17:36:49 +0800

    Add notice before slow memory init

--- kernel/main.c
+++ kernel/main.c
@@ -6,6 +6,7 @@ void main(void) {
   printf("Hello OS from riscv-os (Experiment 1)\n");
 
   /* 页表与 Sv39（Exp3） */
+  printf("[INFO] Initializing physical page allocator (takes a moment) ...\n");
   kinit();
   kvminit();
   kvminithart();
